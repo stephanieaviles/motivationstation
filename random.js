8bdf769bf67c0de590ffe61af96f7b4fa71581a7
Stephanie Aviles <avilestephcode@gmail.com>
2015-06-25 19:02:30 -0400

    created single sentence with changing last words using jquery

---
+++ random.js
@@ -0,0 +1,33 @@
+(function() {
+	var randomWords  = ["MOTIVATED!", "COOL!", "AMAZING!", "HILARIOUS!", "BEAUTIFUL!", "HAPPY!","ALIVE!", "ADMIRABLE!", "INCREDIBLE!", "GENUINE!", "STRONG!", "POWERFUL!", "CAPABLE!"];
+	var arrColor     = ["#45c1bf", "#f0593e", "#aeacd4", "#bdd630", "#4479bd", "#f5b11e", "#70DB70", "#FFA347", "#A34747", "#FFFF66", "#AD5CFF"];
+	var wordSelector = "#changingword";
+	var speed 			 = 200;
+
+	function randomNumber(highest) {
+		return Math.floor(Math.random() * highest);
+	};
+
+	function generatingWords() {
+		var randomIndex = randomNumber(randomWords.length);
+
+		$(wordSelector).fadeOut(speed, function() {
+			$(wordSelector).html(randomWords[randomIndex]);	
+			$(wordSelector).fadeIn(speed);
+		});
+	};
+
+	function changingColors() {
+		var randomIndex = randomNumber(arrColor.length);
+
+		$(wordSelector).fadeOut(speed, function() {
+			$(wordSelector).css("color", arrColor[randomIndex]);	
+			$(wordSelector).fadeIn(speed);
+		});
+	};
+
+	setInterval(generatingWords, 2000);
+	setInterval(changingColors, 2000);
+})();
+
+
